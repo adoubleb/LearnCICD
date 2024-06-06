3e1dc0b36dd010ecdda22ae902497f789c738e1f
adoubleb <85162617+adoubleb@users.noreply.github.com>
2024-06-06 14:44:57 +0800

    Initial Commit

---
+++ learnCICD/learnCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnCICDApp.swift
+//  learnCICD
+//
+//  Created by Abdul Hakam on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct learnCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
